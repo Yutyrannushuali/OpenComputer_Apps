cdb80076382146c1d08b408fb384d40545f99657
Yutyrannus <liusiyu20040211@163.com>
2026-04-11 22:53:07 +0800

    UPDATE ic2_reactor_ctrl.lua

--- src/ic2_reactor_ctrl/ic2_reactor_ctrl.lua
+++ src/ic2_reactor_ctrl/ic2_reactor_ctrl.lua
@@ -1,14 +1,19 @@
 local component = require("component")
 local term = require("term")
-local event = require("event")
 local sides = require("sides")
+local keyboard = require("keyboard")
 local gpu = component.gpu
 local reactor = component.reactor_chamber
 local transposer = component.transposer
 local redstone = component.redstone
 
 
-local isExit = 0
+
+
+-- 快捷键列表
+local cmd_list = '退出: [Ctrl+C] 开始: [Ctrl+R] 暂停: [Ctrl+S]'
+-- 控制程序是否运行
+local isRun = false
 -- 反应堆位于转运器的rc_side方向，资源容器位于src_side，垃圾存放容器位于bin_side
 local rc_side, src_side, bin_side = sides.west, sides.south, sides.north
 -- 反应堆总槽位数，资源箱总槽位数，垃圾箱总槽位数
@@ -28,6 +33,8 @@ local item_list = {
 local rs_side = sides.west
 -- 反应堆温度百分比上限(0~1)
 local heat_rate_limit = 0.5
+
+
 
 
 -- 查询元素是否在表中
@@ -118,15 +125,29 @@ end
 
 -- 反应堆温度监视器，参数side为输出红石信号的方向，heat为反应堆温度上限
 local function heat_monitor(side, heat_rate)
-    -- 温度监控
+    -- 判断是否在冷却状态
+    local isCooling = false
+    -- 获取温度数据
     local now_heat = reactor.getHeat()
     local max_heat = reactor.getMaxHeat()
     local now_heat_rate = now_heat/max_heat
-    if now_heat_rate > heat_rate then
-        redstone.setOutput(side, 0)
-        
-    else
-        redstone.setOutput(side, 15)
+    -- 如果控制程序正在运行
+    if isRun then
+        -- 如果在冷却状态
+        if isCooling then
+            -- 温度百分比低于上限的10%则开启反应堆
+            if now_heat_rate < heat_rate_limit - 0.1 then
+                redstone.setOutput(side, 15)
+                isCooling = false
+            end
+        -- 如果不在冷却状态
+        else
+            -- 温度百分比超过上限则关闭反应堆
+            if now_heat_rate > heat_rate then
+                redstone.setOutput(side, 0)
+                isCooling = true
+            end
+        end
     end
     -- 返回值为当前温度数值，当前温度百分比
     return now_heat, now_heat_rate
@@ -143,27 +164,31 @@ local function item_monitor(slot)
     if slot_info ~= nil then
         -- 获取组件名称
         item_name = item(slot_info)
-        -- 计算已损失耐久
-        if slot_info['damage'] >= slot_info['maxDamage'] then
-            damage = 1
-        else
-            damage = slot_info['damage']/slot_info['maxDamage']
-        end
-        -- 判断是否要更换
-        if IsInTable(slot_info, item_list) then
-            -- 在item_list中的组件，耐久值完全耗尽才更换
-            if damage >= 1 then
-                change(slot_info['name'], slot)
+        -- 如果控制程序正在运行
+        if isRun then
+            -- 计算已损失耐久
+            if slot_info['damage'] >= slot_info['maxDamage'] then
+                damage = 1
+            else
+                damage = slot_info['damage']/slot_info['maxDamage']
             end
-        else
-            -- 不在item_list中的组件，耐久值消耗90%才更换
-            if damage >= 0.9 then
-                change(slot_info['name'], slot)
+            -- 判断是否要更换
+            if IsInTable(slot_info, item_list) then
+                -- 在item_list中的组件，耐久值完全耗尽才更换
+                if damage >= 1 then
+                    change(slot_info['name'], slot)
+                end
+            else
+                -- 不在item_list中的组件，耐久值消耗90%才更换
+                if damage >= 0.9 then
+                    change(slot_info['name'], slot)
+                end
             end
         end
     end
     return damage, item_name
 end
+
 
 -- 反应堆能量监视器
 local function EU_monitor()
@@ -173,67 +198,77 @@ end
 
 -- 组件监视器GUI，显示在第1~6行
 local function item_gui()
-    --while true do
-        local col_cnt = 1
-        local row_cnt = 1
-        for i = 1, reactor_size do
-            local damage, item_name = item_monitor(i)
-            gpu.set(2+(col_cnt-1)*5, row_cnt, '['..item_name..']')
-            col_cnt = col_cnt + 1
-            if col_cnt > col_num then
-                col_cnt = 1
-                row_cnt = row_cnt + 1
-            end
+    local col_cnt = 1
+    local row_cnt = 1
+    for i = 1, reactor_size do
+        local damage, item_name = item_monitor(i)
+        -- 显示名称
+        gpu.set(2+(col_cnt-1)*5, row_cnt*2-1, '['..item_name..']')
+        -- 显示耐久
+        if item_name == '   ' then
+            gpu.set(2+(col_cnt-1)*5, row_cnt*2, '['..'   '..']')
+        else
+            gpu.set(2+(col_cnt-1)*5, row_cnt*2, '['..string.format('%.1f', 1-damage)..']')
         end
-    --end
+        col_cnt = col_cnt + 1
+        if col_cnt > col_num then
+            col_cnt = 1
+            row_cnt = row_cnt + 1
+        end
+    end
 end
 
 -- 温度监视器GUI，显示在7，8行
 local function heat_gui()
-    --while true do
-        local heat, heat_rate = heat_monitor(rs_side, heat_rate_limit)
-        gpu.set(2, 7, '温度: ' .. heat)
-        gpu.set(2, 8, '温度(%): ' .. heat_rate*100 .. '%')
-    --end
+    local heat, heat_rate = heat_monitor(rs_side, heat_rate_limit)
+    gpu.set(2, 13, '温度: ' .. heat)
+    gpu.set(2, 14, '温度(%): ' .. heat_rate*100 .. '%')
 end
 
 -- 其他数据GUI，显示在第7、8行
 local function other_gui()
-    --while true do
-        local EU = EU_monitor()
-        gpu.set(22, 7, string.format('输出: %f EU/t', EU))
-        if reactor.producesEnergy() then
-            gpu.set(22, 8, '状态: 运行')
-        else
-            gpu.set(22, 8, '状态: 停止')
-        end
-    --end
+    local EU = EU_monitor()
+    gpu.set(22, 13, string.format('输出: %f EU/t', EU))
+    if reactor.producesEnergy() then
+        gpu.set(22, 15, '反应堆: 运行')
+    else
+        gpu.set(22, 15, '反应堆: 停止')
+    end
+    if isRun then
+        gpu.set(2, 15, '控制程序: 运行')
+    else
+        gpu.set(2, 15, '控制程序: 停止')
+    end
 end
+
+
 
 
 -- 设置分辨率为50x16
 -- gpu.setResolution(50, 16)
 -- 清空屏幕
 term.clear()
--- 将光标设置在最后一行
+-- 在最后一行显示快捷键
 term.setCursor(1, 16)
+term.write(cmd_list)
 
 
-while isExit == 0 do
+while true do
     item_gui()
     heat_gui()
     other_gui()
-    local _, _, char, code = event.pull(0.2, "key_down")
-    if char then -- 如果捕获到了按键
-        -- char 是 ASCII 码: r=114, s=115, x=120
-        if char == 114 then -- 按 R
-            redstone.setOutput(rs_side, 15)
-            -- 可选：在屏幕打印提示 "Running..."
-        elseif char == 115 then -- 按 S
-            redstone.setOutput(rs_side, 0)
-            -- 可选：在屏幕打印提示 "Stopped"
-        elseif char == 120 then -- 按 X
-            isExit = 1
-        end
+    -- 判断按了哪些键
+    local isCtrl = keyboard.isControlDown()
+    local isC, isR, isS = keyboard.isKeyDown(46), keyboard.isKeyDown(19), keyboard.isKeyDown(31)
+    -- Ctrl+C为“退出”
+    if isCtrl and isC then
+        os.exit(0)
+    -- Ctrl+R为“运行控制程序”
+    elseif isCtrl and isR then
+        isRun = true
+    -- Ctrl+S为“暂停控制程序”
+    elseif isCtrl and isS then
+        isRun = false
     end
+    os.sleep(0)
 end
